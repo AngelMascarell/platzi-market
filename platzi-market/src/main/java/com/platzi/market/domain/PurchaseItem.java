7b31b255cf2bb27810430a9f79b5b6568d114894
Angel <angel.mascarell.tf@gmail.com>
2023-06-24 19:12:12 +0200

    purchase repositorio y http

---
+++ platzi-market/src/main/java/com/platzi/market/domain/PurchaseItem.java
@@ -0,0 +1,46 @@
+package com.platzi.market.domain;
+
+import com.sun.jdi.request.StepRequest;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class PurchaseItem {
+
+    private int productId;
+    private int quantity;
+    private double total;
+    private boolean active;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
